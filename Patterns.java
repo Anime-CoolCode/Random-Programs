d7dbd020eb48a1cc97aadbfdb4e2e95488de3f80
Anime-CoolCode <singh.animesh.tanwar@gmail.com>
2024-07-15 19:45:08 +0530

    Advanced Patterns no 1

--- Patterns.java
+++ Patterns.java
@@ -1,14 +1,96 @@
+
+/*
+ * Targer to print 
+ * *      *
+ * **    **
+ * ***  ***
+ * ********
+ * ********
+ * ***  ***
+ * **    **
+ * *      *
+ * 
+ * using Loops
+ */
+
 class Patterns{
-    public static void main(String[] args){
-
-        int lines = 5;
-        int rows = 10;
-
-        for(int l = 1; l<=lines ; l++){
-            for (int r = 0; r < l ;r++){
-                System.out.print("* ");
+/*Quater 3
+        for (int l = 1 ; l <= lines ; l ++  ){
+            for (int r = 1 ; r <= lines  ;r++){
+                if (r <= (lines - l+1)) {
+                    System.out.print("* ");
+                }
+            }
+            System.out.println( );
+        }
+ Quater 2
+        for(int l = 1; l <= lines ; l++){
+            for(int r = 1; r <= lines ; r++){
+                if(r >= (lines-l+1)){
+                    System.out.print("* ");
+                }else{
+                    System.out.print("  ");
+                }
             }
             System.out.println();
         }
+        System.out.println("Hey");
+ Quater 4
+        for (int l = 1 ; l <= lines ; l ++  ){
+            for (int r = 1 ; r <= lines  ;r++){
+                if (r > (l-1)) {
+                    System.out.print("* ");
+                }else{
+                    System.out.print("  ");
+                }
+            }
+            System.out.println( );
+        }
+    }
+*/
+    public static void main(String[] args){
+        
+        int lines = 9;
+
+        
+        for(int l = 1;l <= lines; l++){
+            for(int r = 1; r <= lines ; r++){
+                    
+                    //Quater 1
+                    if((r <= (lines/2)) && (l <= (lines/2))){
+                        if (r <= l){
+                            System.out.print("* ");
+                        }else System.out.print("  ");
+                    }
+                    //Quater 2
+                    else if((r > (lines/2)) && (l <= (lines/2))){
+                        if(r >= (lines-l+1)){
+                            System.out.print("* ");
+                        }else{
+                            System.out.print("  ");
+                        }
+                    }
+                    //Quater 3                              You were here
+                    else if((r <= (lines/2)) && (l > (lines/2))){
+                        if (r <= (lines - l+1)) {
+                            System.out.print("* ");
+                        }
+                        else {
+                            System.out.print("  ");
+                        }
+                    }
+                    //Quater 4
+                    else{
+                        if (r > (l-1)) {
+                            System.out.print("* ");
+                        }else{
+                            System.out.print("  ");
+                        }
+                    }
+                }
+                System.out.println();
+            }
+        //}
+
     }
 }
