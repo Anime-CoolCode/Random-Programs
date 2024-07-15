1b4800218763d72f3544dffb3f8fe402306a122e
Anime-CoolCode <singh.animesh.tanwar@gmail.com>
2024-07-15 17:28:09 +0530

    Pattern 2

--- Patterns.java
+++ Patterns.java
@@ -1,20 +1,12 @@
 class Patterns{
     public static void main(String[] args){
 
-        int lines = 10;
+        int lines = 5;
         int rows = 10;
 
-        for(int l = 0; l<lines ; l++){
-            for (int r = 0; r < rows ;r++){
-                if((l == 0)||(l==(lines-1))){
-                    System.out.print("* ");
-                }else{
-                    if((r==0)||(r==(rows-1))){
-                        System.out.print("* ");
-                    }else{
-                        System.out.print("  ");
-                    }
-                }
+        for(int l = 1; l<=lines ; l++){
+            for (int r = 0; r < l ;r++){
+                System.out.print("* ");
             }
             System.out.println();
         }
