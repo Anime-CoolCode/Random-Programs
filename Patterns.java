bde5883dd6741405eb1d385ac0fa7807274af082
Anime-CoolCode <singh.animesh.tanwar@gmail.com>
2024-07-15 22:12:22 +0530

    Advanced Pattern 2

--- Patterns.java
+++ Patterns.java
@@ -1,96 +1,31 @@
 
 /*
  * Targer to print 
- * *      *
- * **    **
- * ***  ***
- * ********
- * ********
- * ***  ***
- * **    **
- * *      *
+ *     *****
+ *    *****
+ *   *****
+ *  *****
+ * *****
  * 
  * using Loops
  */
 
 class Patterns{
-/*Quater 3
-        for (int l = 1 ; l <= lines ; l ++  ){
-            for (int r = 1 ; r <= lines  ;r++){
-                if (r <= (lines - l+1)) {
-                    System.out.print("* ");
-                }
-            }
-            System.out.println( );
-        }
- Quater 2
-        for(int l = 1; l <= lines ; l++){
-            for(int r = 1; r <= lines ; r++){
-                if(r >= (lines-l+1)){
-                    System.out.print("* ");
-                }else{
-                    System.out.print("  ");
-                }
-            }
-            System.out.println();
-        }
-        System.out.println("Hey");
- Quater 4
-        for (int l = 1 ; l <= lines ; l ++  ){
-            for (int r = 1 ; r <= lines  ;r++){
-                if (r > (l-1)) {
-                    System.out.print("* ");
-                }else{
-                    System.out.print("  ");
-                }
-            }
-            System.out.println( );
-        }
-    }
-*/
     public static void main(String[] args){
         
-        int lines = 9;
+        int lines = 5;
 
         
         for(int l = 1;l <= lines; l++){
-            for(int r = 1; r <= lines ; r++){
-                    
-                    //Quater 1
-                    if((r <= (lines/2)) && (l <= (lines/2))){
-                        if (r <= l){
-                            System.out.print("* ");
-                        }else System.out.print("  ");
-                    }
-                    //Quater 2
-                    else if((r > (lines/2)) && (l <= (lines/2))){
-                        if(r >= (lines-l+1)){
-                            System.out.print("* ");
-                        }else{
-                            System.out.print("  ");
-                        }
-                    }
-                    //Quater 3                              You were here
-                    else if((r <= (lines/2)) && (l > (lines/2))){
-                        if (r <= (lines - l+1)) {
-                            System.out.print("* ");
-                        }
-                        else {
-                            System.out.print("  ");
-                        }
-                    }
-                    //Quater 4
-                    else{
-                        if (r > (l-1)) {
-                            System.out.print("* ");
-                        }else{
-                            System.out.print("  ");
-                        }
-                    }
-                }
-                System.out.println();
+            for(int r = 1; r <= (lines+(lines-1)) ; r++){
+              if(r <= (lines-l) || ((l+r) >= (lines+lines+1))){
+                System.out.print(" ");
+              }else{
+                System.out.print("*");
+              }
             }
-        //}
+            System.out.println();
+        }
 
     }
 }
